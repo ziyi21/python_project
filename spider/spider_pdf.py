678802f50580b2fb7807e9def42ba97240a78379
ziyi <635516607@qq.com>
2018-04-14 13:07:01 +0800

    增加澎湃新闻
    Signed-off-by: ziyi <635516607@qq.com>

--- spider/spider_pdf.py
+++ spider/spider_pdf.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ziyi'
 
+import requests
+from urllib import request
 def getFile(url):
     file_name = url.split('/')[-1]
-    u = urllib.request.urlopen(url)
-    f = open(file_name, 'wb')
-
+    u = request.urlopen(url)
+    f = open('pdf_record/'+file_name, 'wb')
     block_sz = 8192
     while True:
         buffer = u.read(block_sz)
@@ -15,4 +16,7 @@ def getFile(url):
 
         f.write(buffer)
     f.close()
-    print ("Sucessful to download" + " " + file_name)
+    print ("Sucessful to 'download" + " " + file_name)
+
+url = 'http://disclosure.szse.cn/finalpage/2018-04-09/1204593820.PDF'
+getFile(url)
